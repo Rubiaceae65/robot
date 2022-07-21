0a4f311f8da20fd366e6e92e5c8f49e5b57caefc
Your Name <you@example.com>
2022-07-21 22:46:08 +0000

    imu seems to work

--- ros2-workspace/src/mpu9250_python/mpu9250_python/mpu_node.py
+++ ros2-workspace/src/mpu9250_python/mpu9250_python/mpu_node.py
@@ -17,6 +17,9 @@ from geometry_msgs.msg import Quaternion
 import time
 from mpu9250_jmdev.registers import *
 from mpu9250_jmdev.mpu_9250 import MPU9250
+G = 9.80665
+MagFieldConversion_uT_T = 0.000001
+import math
 #from tf.transformations import quaternion_about_axis
 debug = False
 
@@ -48,9 +51,13 @@ class MpuPublisher(Node):
 
 
         # todo make this parameters, and do some calibration service or something
-        #self.mpu.abias = [0.024116330030487805, 0.09404177782012195, -0.20670374428353655]
-        #self.mpu.gbias =  [4.080423494664634, -6.630874261623475, -5.128627870141006]
+        # accell bias
+        self.mpu.abias = [0.024116330030487805, 0.09404177782012195, -0.20670374428353655]
+        # gyro bias
+        self.mpu.gbias =  [4.080423494664634, -6.630874261623475, -5.128627870141006]
+        #soft iron distortion
         #self.mpu.magScale =  [1.2469135802469136, 0.8632478632478632, 0.9619047619047618]
+        # hard iron distortionm
         #self.mpu.mbias =  [17.125546588827838, 24.838324175824177, 17.700995879120878]
 
         #self.mpu.abias = [-0.08004239710365854, 0.458740234375, 0.2116996951219512]
@@ -58,20 +65,12 @@ class MpuPublisher(Node):
         #self.mpu.magScale = [1.0104166666666667, 0.9797979797979799, 1.0104166666666667]
         #self.mpu.mbias = [2.6989010989010986, 2.7832417582417586, 2.6989010989010986]
 
-
-
-        #self.declare_parameter('variance_acc', value=registers.DEFAULT_VARIANCE_ACC)
-        #self.declare_parameter('variance_angular_vel', value=registers.DEFAULT_VARIANCE_ANGULAR_VEL)
-        #self.declare_parameter('variance_orientation', value=registers.DEFAULT_VARIANCE_ORIENTATION)
-        #self.declare_parameter('variance_mag', value=registers.DEFAULT_VARIANCE_MAG)
-
         time.sleep(1) # delay necessary to allow mpu9250 to settle
 
         QoSProf = QoSProfile(depth=10)
         prefix = "imu/"
         # create topic publishers:
         self.pub_imu_raw = self.create_publisher(Imu, prefix + 'data_raw', QoSProf)
-        #self.pub_imu = self.create_publisher(Imu, prefix + 'imu', QoSProf)
         self.pub_mag = self.create_publisher(MagneticField, prefix + 'mag', QoSProf)
         self.pub_temp = self.create_publisher(Temperature, prefix + 'temp', QoSProf)
         #self.pub_calib_status = node.create_publisher(String, prefix + 'calib_status', QoSProf)
@@ -92,15 +91,15 @@ class MpuPublisher(Node):
             print("Gyroscope", self.mpu.readGyroscopeMaster())
             print("Magnetometer", self.mpu.readMagnetometerMaster())
             print("Temperature", self.mpu.readTemperatureMaster())
-        #a = self.mpu.readAccelerometerMaster()
-        #g = self.mpu.readGyroscopeMaster()
-        #m = self.mpu.readMagnetometerMaster()
-        #t = self.mpu.readTemperatureMaster()
-
-#['timestamp', 'master_acc_x', 'master_acc_y', 'master_acc_z', 'master_gyro_x', 'master_gyro_y', 'master_gyro_z', 'slave_acc_x', 'slave_acc_y', 'slave_acc_z', 'slave_gyro_x', 'slave_gyro_y', 'slave_gyro_z', 'mag_x', 'mag_y', 'mag_z', 'master_temp', 'slave_temp']
+        #['timestamp', 'master_acc_x', 'master_acc_y', 'master_acc_z', 'master_gyro_x', 'master_gyro_y', 'master_gyro_z', 'slave_acc_x', 'slave_acc_y', 'slave_acc_z', 'slave_gyro_x', 'slave_gyro_y', 'slave_gyro_z', 'mag_x', 'mag_y', 'mag_z', 'master_temp', 'slave_temp']
 #[1658436898.8061285, 0.020751953125, 0.097900390625, 0.786376953125, 4.119873046875, -6.561279296875, -5.126953125, 0, 0, 0, 0, 0, 0, 16.68187957875458, 25.639560439560437, 17.102411477411476, 43.90711953754455, 0]
         labels = self.mpu.getAllDataLabels() # return labels with data description for each array position
         d = self.mpu.getAllData() # returns a array with data from all sensors
+        
+        #Accelerometer: g (1g = 9.80665 m/s²)
+        #Gyroscope:   degrees per second (°/s)
+        #Magnetometer:   microtesla (μT)
+        #Temperature:  celsius degrees (°C)
 
         tsmpu = d[0]
         ax = d[1]
@@ -114,11 +113,6 @@ class MpuPublisher(Node):
         mz = d[15]
         temp = d[16]
                 
-        #try:
-        #    ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
-        #    mx,my,mz = AK8963_conv() # read and convert AK8963 magnetometer data
-        #except:
-        #    print("failure")
        
         if debug:
             print('{}'.format('-'*30))
@@ -132,57 +126,52 @@ class MpuPublisher(Node):
         frame_id = self.get_parameter("frame_id").value
 
         imu_raw_msg = Imu()
-        #imu_msg = Imu()
         mag_msg = MagneticField()
         temp_msg = Temperature() 
         temp_msg.header.stamp = stamp
         temp_msg.header.frame_id = frame_id
-        # temp_msg.header.seq = seq
         temp_msg.temperature = temp
         self.pub_temp.publish(temp_msg)
 
         imu_raw_msg.header.stamp = stamp
         imu_raw_msg.header.frame_id = frame_id
 
-        ## Calculate a quaternion representing the orientation
-        #accel = accel_x, accel_y, accel_z
-        #ref = np.array([0, 0, 1])
-        #acceln = accel / np.linalg.norm(accel)
-        #axis = np.cross(acceln, ref)
-        #angle = np.arccos(np.dot(acceln, ref))
-        #orientation = quaternion_about_axis(angle, axis)
+        # copied from: https://github.com/bjajoh/ros-mpu9250-ahrs/blob/main/mpu9250-driver/scripts/mpu9250.py (why those numbers??)
+        imu_raw_msg.orientation.x = 0.0
+        imu_raw_msg.orientation.y = 0.0
+        imu_raw_msg.orientation.z = 0.0
+        imu_raw_msg.orientation.w = 0.0
+        imu_raw_msg.orientation_covariance[0] = 0.01
+        imu_raw_msg.orientation_covariance[4] = 0.01
+        imu_raw_msg.orientation_covariance[8] = 0.01
 
-        #imu_raw_msg.orientation_covariance = [
-        #    self.param.variance_orientation.value[0], 0.0, 0.0,
-        #    0.0, self.param.variance_orientation.value[1], 0.0,
-        #    0.0, 0.0, self.param.variance_orientation.value[2]
-        #]       
+        imu_raw_msg.linear_acceleration.x = ax*G
+        imu_raw_msg.linear_acceleration.y = ay*G
+        imu_raw_msg.linear_acceleration.z = az*G
+        # why 10??
+        imu_raw_msg.linear_acceleration_covariance[0] = 10
+        imu_raw_msg.linear_acceleration_covariance[4] = 10
+        imu_raw_msg.linear_acceleration_covariance[8] = 10
 
-        imu_raw_msg.linear_acceleration.x = ax
-        imu_raw_msg.linear_acceleration.y = ay
-        imu_raw_msg.linear_acceleration.z = az
-        #imu_raw_msg.linear_acceleration_covariance = [
-        #    self.param.variance_acc.value[0], 0.0, 0.0,
-        #    0.0, self.param.variance_acc.value[1], 0.0,
-        #    0.0, 0.0, self.param.variance_acc.value[2]
-        #]       
-        imu_raw_msg.angular_velocity.x = wx
-        imu_raw_msg.angular_velocity.y = wy
-        imu_raw_msg.angular_velocity.z = wz
+        imu_raw_msg.angular_velocity.x = math.radians(wx)
+        imu_raw_msg.angular_velocity.y = math.radians(wy)
+        imu_raw_msg.angular_velocity.z = math.radians(wz)
+        # why 0.03???
+        imu_raw_msg.angular_velocity_covariance[0] = 0.03
+        imu_raw_msg.angular_velocity_covariance[4] = 0.03
+        imu_raw_msg.angular_velocity_covariance[8] = 0.03
         self.pub_imu_raw.publish(imu_raw_msg) 
 
         # Publish magnetometer data
         mag_msg.header.stamp = stamp
         mag_msg.header.frame_id = frame_id
         # mag_msg.header.seq = seq
-        mag_msg.magnetic_field.x = mx
-        mag_msg.magnetic_field.y = my
-        mag_msg.magnetic_field.z = mz
-        #mag_msg.magnetic_field_covariance = [
-        #    self.param.variance_mag.value[0], 0.0, 0.0,
-        #    0.0, self.param.variance_mag.value[1], 0.0,
-        #    0.0, 0.0, self.param.variance_mag.value[2]
-        #]
+        mag_msg.magnetic_field.x = mx*MagFieldConversion_uT_T
+        mag_msg.magnetic_field.y = my*MagFieldConversion_uT_T
+        mag_msg.magnetic_field.z = mz*MagFieldConversion_uT_T
+
+
+
         self.pub_mag.publish(mag_msg)
  
 def main(args=None):
