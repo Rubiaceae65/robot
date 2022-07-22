842acea2a87cd09401ecd05e9f79b9ffb96c207a
Your Name <user@robot6500>
2022-07-22 12:41:54 +0000

    la

--- ros2-workspace/src/robot2_description/launch/headless.launch.py
+++ ros2-workspace/src/robot2_description/launch/headless.launch.py
@@ -1,50 +1,43 @@
 import launch
-from launch_ros.actions import Node
 from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
 import os
+from launch_ros.actions import Node
 
 def generate_launch_description():
     
 
     pkg_share = launch_ros.substitutions.FindPackageShare(package='robot2_description').find('robot2_description')
     default_model_path = os.path.join(pkg_share, 'src/description/robot2.urdf.xacro')
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
-
+    robot_localization_file_path = os.path.join(pkg_share, 'config/ekf.yaml') 
+ 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description':launch_ros.parameter_descriptions.ParameterValue(Command(['xacro ', LaunchConfiguration('model')]), value_type=str)}]
     )
+
+   
+
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-    )
-    rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
+      # Start robot localization using an Extended Kalman filter
+    robot_localization = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
         output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
-    )
+        parameters=[robot_localization_file_path])
+
 
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='False',
-                                            description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                            description='Absolute path to rviz config file'),
-        joint_state_publisher_node,
-        joint_state_publisher_gui_node,
+
         robot_state_publisher_node,
-        rviz_node
+  #      joint_state_publisher_node,
+        robot_localization
     ])
